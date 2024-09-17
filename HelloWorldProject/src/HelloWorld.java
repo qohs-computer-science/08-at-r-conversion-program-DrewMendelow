f6c9c1ae542331ac6f196a15d2165eaabb5d6d89
DrewMendelow <103079010+DrewMendelow@users.noreply.github.com>
2024-09-17 17:39:32 +0000

    still working 9/17

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -1,13 +1,71 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * Drew Mendelow
+ * 9/17/2024
+ * pd 7
+ * asks user for number and converts it from decimal to binary or other way
  */
+import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		//TODO: Implement Program Requirements Here
+		Scanner input = new Scanner(System.in);
+		
+		boolean run = true;
+		while (run){
+			int answer = 0;
+			System.out.println("Welcome!  You have the following options:\n1. Binary to Decimal\n2. Decimal to Binary");
+			while (answer != 1 && answer != 2){
+				System.out.println("Which type of conversion would you like to perform?");
+				answer = input.nextInt();
+				input.nextLine();
+			}
+
+			if (answer == 1){
+				System.out.println("What is the binary number?");
+				int binary = input.nextInt();
+				input.nextLine();
+
+				int power =  0;
+				int decimal = 0;
+				while (binary > 0){
+					if (binary % 10 == 1){
+						decimal += Math.pow(2, power);
+					}
+					binary /= 10;
+					power++;
+				}
+				System.out.println("The decimal equivalent is: " + decimal);
+			}
+			else{
+				System.out.println("What is the decimal number?");
+				int decimal = input.nextInt();
+				input.nextLine();
+
+				String binary = "";
+				while (decimal > 0){
+					binary = (decimal % 2) + binary;
+					decimal /= 2;
+				}
+				System.out.println("The binary equivalent is: " + binary);
+			}
+
+			System.out.println("Do you want to continue - yes or no?");
+			
+			String ans = input.nextLine().trim().toLowerCase();
+			// while (response != "yes" && response != "no"){
+			// 	System.out.println("Do you want to continue - yes or no?");
+			// 	response = input.nextLine();
+			// }
+
+			if (ans.equals("yes")){
+				run = true;
+				System.out.println("\n\n");
+			}
+			else{
+				run = false;
+			}
+		}
+		input.close();
 	}
 
 }
